3dc7fdc3e8c2d9255907acc9049090a036214cbf
paramatman <46036085+paramatman@users.noreply.github.com>
2019-05-09 03:56:22 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,12 +1,40 @@
 $( document ).ready(function() {
     	
+	var recorder;	
 	// wait and display login screen
 	
 	setTimeout(function(){
 		changePage("login.html");
 	},500);
-			
+	
+	$(document).on("pageshow","#recordScreen",function(){
+		
+		navigator.mediaDevices.getUserMedia({ audio: true })
+			.then(function (stream) {
+				recorder = new MediaRecorder(stream);
+
+				recorder.addEventListener('dataavailable', onRecordingReady);
+		});
+		
+	});
+		
+	$(document).on("click","#record",function(event){
+		recorder.start();		
+	});
+	
+	$(document).on("click","#stop",function(event){
+		recorder.stop();		
+	});
+	
 });
+
+function onRecordingReady(e) {
+  var audio = document.getElementById('recording');
+  
+  audio.src = URL.createObjectURL(e.data);
+  
+  audio.play();
+}
 
 function authenticateUser()
 {
@@ -30,3 +58,5 @@ function changePage(url)
 {
 	$.mobile.changePage(url);
 }
+
+	
